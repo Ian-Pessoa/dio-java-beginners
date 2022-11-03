adc215ee756746ab4fbe98c507689ed0ea80451c
Ian <ian.pessoaa@gmail.com>
2022-11-02 21:25:33 -0300

    Adição exercico de metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        smartTv.novoCanal(13);
+
+        System.out.println("TV Ligada " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+    }
+}
