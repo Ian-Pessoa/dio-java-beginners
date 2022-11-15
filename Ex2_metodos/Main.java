155ac8f2a650ff04590150d7c58a5ed7f4d5ff79
Ian Pessôa <105527360+Ian-Pessoa@users.noreply.github.com>
2022-11-15 01:10:33 -0300

    2º Exercício de métodos, utilizando sobrecarga

---
+++ Ex2_metodos/Main.java
@@ -0,0 +1,13 @@
+package Ex2_metodos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("\nExercício de quadrilátero:");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+
+    }
+}
